64a1c9217ff5057ad68c1f35fc61033d2673cdb7
valcroft <valerieaurum@gmail.com>
2016-02-08 22:26:14 +0800

    gumagana na si intro sa laptop :)))

--- js/body.js
+++ js/body.js
@@ -1,16 +1,60 @@
 jQuery(document).ready(function($) {
-//   $(function(){ //DOM Ready
-//   var gridster = $(".gridster ul").gridster().data('gridster');
 
+    $('div.main-square').hide();
+    $('div.centered-text').hide();
+    
+    var i =0;
+    
     $(".gridster ul").gridster({
         widget_margins: [10, 10],
         widget_base_dimensions: [200, 200]
     });
     
- //   $("#upper-right").gridster().data('gridster').disable();
+    if (i ==0) {
+        $('div.notif-first').show(250);
 
-  //  });
+        $('div.notif-first').animate({"font-size": "2em"},
+            {duration:2000, queue: true,
+            complete: function() { 
+        }});
+         $( 'div.text-overlay' ).colorFade( [26,113,215]); 
+
+         $('div.notif-first').animate({"font-size": "1.75em"},
+                {duration:2000, queue: true,
+            complete: function() { /* Animation complete */  
+            $('div.notif-first').hide(0); 
+                      $('div.notif-second').show(0);
+                       $('div.notif-second').animate({"font-size": "2em"},
+                        {duration:2000, queue: true,
+                        complete: function() { 
+                    }});
+                     $( 'div.text-overlay' ).colorFade( [26,113,215]); 
+
+                     $('div.notif-second').animate({"font-size": "1.75em"},
+                            {duration:2000, queue: true,
+                        complete: function() { /* Animation complete */  
+                            $('div.notif-second').hide(0); 
+                            $('div.main-square').show(0);
+                            $('div#intro-first').hide(0);
+
+                        }
+                    });
+                  i++;
+            }
+         });
+             
+  
+          
+      
+                 
+    }
+        
+           // $('div#intro-first').hide();
+      
+    
     
    //  var gridster = $('.gridster ul').gridster().data('gridster');
-    
+    $('.rc-11').click(function() {	
+        $(this).removeClass('locked');   
+    });
 });
