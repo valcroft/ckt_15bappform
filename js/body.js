bbb0a4d68f61f7d34ff5ab0ef1cefde493df6d0e
valcroft <valerieaurum@gmail.com>
2016-02-08 14:26:43 +0800

    bakit ayaw magrollback save change
    yung may changes from demo site

--- js/body.js
+++ js/body.js
@@ -6,8 +6,7 @@ jQuery(document).ready(function($) {
         widget_margins: [10, 10],
         widget_base_dimensions: [200, 200]
     });
-gridster.add_widget('<li class="new">The HTML of the widget...</li>', 1,1 ,3, 4);
+
     });
-    
     
 });
