72f07dc03d5b9f14e36679a0a6b8cab132e1cad0
valcroft <valerieaurum@gmail.com>
2016-02-08 16:11:03 +0800

    WORKING TILES LIKE OMG

--- js/body.js
+++ js/body.js
@@ -1,12 +1,16 @@
 jQuery(document).ready(function($) {
-   $(function(){ //DOM Ready
-   var gridster = $(".gridster ul").gridster().data('gridster');
+//   $(function(){ //DOM Ready
+//   var gridster = $(".gridster ul").gridster().data('gridster');
 
     $(".gridster ul").gridster({
         widget_margins: [10, 10],
         widget_base_dimensions: [200, 200]
     });
+    
+ //   $("#upper-right").gridster().data('gridster').disable();
 
-    });
+  //  });
+    
+   //  var gridster = $('.gridster ul').gridster().data('gridster');
     
 });
